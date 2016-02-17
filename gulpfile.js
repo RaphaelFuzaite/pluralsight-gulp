24f23610e4f9bd2a6dd279ab03b35bd22338b7bf
Raphael Fuzaite <raphaelfuzaite@outlook.com>
2016-02-17 11:20:23 -0200

    Gulp load plugins and config module

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,30 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var config = require('./gulp.config')();
 
-var jshint = require('gulp-jshint');
-var jscs = require('gulp-jscs');
-var util = require('gulp-util');
-var gulpprint = require('gulp-print');
-var gulpif = require('gulp-if');
+var $ = require('gulp-load-plugins')({ lazy: true });
 
 gulp.task('vet', function() {
     log('Analyzing source with JSHint and JSCS');
 	return gulp
-		.src([
-			'./src/**/*.js',
-			'./*.js'
-		])
+		.src(config.alljs)
 		// Passando o argumento --verbose, irá executar a condição do gulp-if,
 		// no caso, exibirá todos os arquivos que estão sendo manipulados
-		.pipe(gulpif(args.verbose, gulpprint()))
-		.pipe(jscs())
-		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish', { verbose: true }))
-		.pipe(jshint.reporter('fail'));
+		.pipe($.if(args.verbose, $.print()))
+		.pipe($.jscs())
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
+		.pipe($.jshint.reporter('fail'));
 });
 
 function log(msg) {
     if (typeof(msg) === 'object') {
 		for (var item in msg) {
 			if (msg.hasOwnProperty(item)) {
-				util.log(util.colors.blue(msg[item]));
+				$.util.log($.util.colors.blue(msg[item]));
 			}
 		}
 	} else {
-		util.log(util.colors.blue(msg));
+		$.util.log($.util.colors.blue(msg));
 	}
 }
