0c26d0ec8c0200bc26ab126bec4ec4e7cf646baa
Raphael Fuzaite <raphaelfuzaite@outlook.com>
2016-02-17 12:07:34 -0200

    CSS Compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config')();
+var del = require('del');
 
 var $ = require('gulp-load-plugins')({ lazy: true });
 
-gulp.task('vet', function() {
+function log(msg) {
+    if (typeof (msg) === 'object') {
+        var item;
+		for (item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				$.util.log($.util.colors.blue(msg[item]));
+			}
+		}
+	} else {
+		$.util.log($.util.colors.blue(msg));
+	}
+}
+
+function clean(path) {
+	log('Cleaning: ' + $.util.colors.blue(path));
+	del(path);
+}
+
+function errorLogger (error) {
+	log('*** Start of Error ***');
+	log(error);
+	log('*** End of Error ***');
+	this.emit('end');
+}
+
+gulp.task('vet', function () {
     log('Analyzing source with JSHint and JSCS');
 	return gulp
 		.src(config.alljs)
@@ -17,14 +43,23 @@ gulp.task('vet', function() {
 		.pipe($.jshint.reporter('fail'));
 });
 
-function log(msg) {
-    if (typeof(msg) === 'object') {
-		for (var item in msg) {
-			if (msg.hasOwnProperty(item)) {
-				$.util.log($.util.colors.blue(msg[item]));
-			}
-		}
-	} else {
-		$.util.log($.util.colors.blue(msg));
-	}
-}
+gulp.task('clean-styles', function () {
+	var files = config.temp + '**/*.css';
+	clean(files);
+});
+
+gulp.task('less-watcher', function() {
+	gulp.watch([config.less], ['styles']);
+});
+
+gulp.task('styles', ['clean-styles'], function () {
+	log('Compiling Less --> CSS');
+	
+	return gulp
+		.src(config.less)
+		.pipe($.plumber())
+		.pipe($.less())
+		//.on('error', errorLogger)
+		.pipe($.autoprefixer({ browsers: ['last 2 versions', '> 5%'] }))
+		.pipe(gulp.dest(config.temp));
+});
