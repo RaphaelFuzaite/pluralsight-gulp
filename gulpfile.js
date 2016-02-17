926ad74eda24a196d24a610c6ced110c34914975
Raphael Fuzaite <raphaelfuzaite@outlook.com>
2016-02-17 14:05:16 -0200

    Inject files and automated browser synchronization

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var browserSync = require('browser-sync');
 var config = require('./gulp.config')();
 var del = require('del');
-
 var $ = require('gulp-load-plugins')({ lazy: true });
+var port = process.env.PORT || config.defaultPort;
 
 function log(msg) {
     if (typeof (msg) === 'object') {
@@ -28,6 +29,48 @@ function errorLogger (error) {
 	log(error);
 	log('*** End of Error ***');
 	this.emit('end');
+}
+
+function changeEvent(event) {
+	var srcPattern = new RegExp('/.*(?=/' + config.source + ')/');
+	log('File ' + event.path.replace(srcPattern, '') + ' ' + event.type);
+} 
+
+function startBrowserSync() {
+	if (args.nosync || browserSync.active) {
+		return;
+	}
+	
+	log('Starting browser-sync on port ' + port);
+	
+	gulp.watch([config.less], ['styles'])
+	.on('change', function (ev) {
+		changeEvent(ev);
+	});
+	
+	var options = {
+		proxy: 'localhost:' + port,
+		port: 3000,
+		files: [
+			config.client + '**/**.*',
+			'!' + config.less,
+			config.temp + '**/**.css'
+		],
+		ghostMode: {
+			clicks: true,
+			location: false,
+			forms: true,
+			scroll: true
+		},
+		injectChanges: true,
+		logFileChanges: true,
+		logLevel: 'debug',
+		logPrefix: 'gulp-patterns',
+		notify: true,
+		reloadDelay: 1000
+	}; 
+	
+	browserSync(options);
 }
 
 gulp.task('vet', function () {
@@ -62,4 +105,59 @@ gulp.task('styles', ['clean-styles'], function () {
 		//.on('error', errorLogger)
 		.pipe($.autoprefixer({ browsers: ['last 2 versions', '> 5%'] }))
 		.pipe(gulp.dest(config.temp));
+});
+
+gulp.task('wiredep', function () {
+	log('Wire up the bower css js and our app js into the html');
+	
+	var wiredep = require('wiredep').stream;
+	var options = config.getWiredepDefaultOptions();
+	return gulp
+		.src(config.index)
+		.pipe(wiredep(options))
+		.pipe($.inject(gulp.src(config.js)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep', 'styles'], function () {
+	log('Inject our app css into the html');
+	
+	return gulp
+		.src(config.index)
+		.pipe($.inject(gulp.src(config.css)))
+		.pipe(gulp.dest(config.client));
+});
+
+gulp.task('serve-dev', ['inject'], function () {
+	var isDev = true;
+	
+	var nodeOptions = {
+		script: config.nodeServer,
+		delatyTime: 1,
+		env: {
+			'PORT': port,
+			'NODE_ENV': isDev ? 'dev' : 'build'
+		},
+		watch: [config.server]	
+	};
+	
+	return $.nodemon(nodeOptions)
+		.on('restart', function (ev) {
+			log('*** nodemon restarted');
+			log('files changed on restart: \n' + ev);
+			setTimeout(function(){
+				browserSync.notify('Reloading now...');
+				browserSync.reload({ stream: false });
+			}, config.browserReloadDelay); 
+		})
+		.on('start', function () {
+			log('*** nodemon started');
+			startBrowserSync();
+		})
+		.on('crash', function () {
+			log('*** nodemon crashed: script crashed for some reason');
+		})
+		.on('exit', function () {
+			log('*** nodemon exited cleanly');
+		});
 });
